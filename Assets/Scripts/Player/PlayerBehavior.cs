460470f980724692cfb1b993c45663eea1525f24
Minseo Jeon <minseo4565@snu.ac.kr>
2022-07-25 22:30:36 +0900

    ADD Wizard Animation

--- Assets/Scripts/Player/PlayerBehavior.cs
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -6,28 +6,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody2D;
-
     private Vector2 _moving;
 
+    private float _attackTime;
+
+    private Rigidbody2D _rigidbody2D;
     private ExpeditionManaging _expeditionManaging;
     private PlayerDataContainer _playerDataContainer;
     private PlayerSkill _playerSkill;
-    
+    private Animator _animator;
+
     public int playerNum;
 
     private void Start()
     {
+        _attackTime = 0f;
+        
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _expeditionManaging = transform.parent.gameObject.GetComponent<ExpeditionManaging>();
         _playerDataContainer = GetComponent<PlayerDataContainer>();
         _playerSkill = GetComponent<PlayerSkill>();
-        
-        _expeditionManaging = transform.parent.gameObject.GetComponent<ExpeditionManaging>();
+        _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        transform.Translate(new Vector3(_moving.x, 0f, 0f) * (Time.deltaTime * _playerDataContainer.speed));
+        _attackTime -= Time.deltaTime;
+        if (_attackTime < 0) _attackTime = 0;
+        if (_attackTime == 0)
+        transform.Translate(new Vector3(_moving.x, 0f, 0f) * (Time.deltaTime * _playerDataContainer.speed)); // 이동 적용
+        
+        if (_rigidbody2D.velocity.y != 0)
+        {
+            _animator.SetBool("isJump", true); 
+        }
+        _animator.SetBool("isRun", _moving.x != 0);
+        if (_moving.x * transform.localScale.x < 0 && _attackTime == 0)
+            transform.localScale
+                = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        
+        
     }
 
     public void OnMovement(InputAction.CallbackContext value)
@@ -38,38 +56,67 @@ public class PlayerBehavior : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext value)
     {
-        if (value.started && _expeditionManaging.CoolTimes[playerNum, 0] <= 0)
+        if (value.started && _attackTime == 0)
         {
-            _rigidbody2D.AddForce(Vector2.up * _playerDataContainer.jumpPower, ForceMode2D.Impulse);
-            _expeditionManaging.CoolTimes[playerNum, 0] = _playerDataContainer.finalCoolTimes[0]; // 쿨타임 적용
-            // _jumpAvailable.Value = false;
+            if (!_animator.GetBool("isJump"))
+                _rigidbody2D.AddForce(Vector2.up * _playerDataContainer.jumpPower, ForceMode2D.Impulse);
         }
     }
 
     public void OnQ(InputAction.CallbackContext value)
     {
-        if (value.started && _expeditionManaging.CoolTimes[playerNum, 0] <= 0 && _playerDataContainer.skillNums.Count >= 1)
+        if (value.started && _expeditionManaging.CoolTimes[playerNum, 0] <= 0 &&
+            _playerDataContainer.skillNums.Count >= 1)
         {
             _playerSkill.Skill(_playerDataContainer.skillNums[0]);
             _expeditionManaging.CoolTimes[playerNum, 0] = _playerDataContainer.finalCoolTimes[0]; // 쿨타임 적용
         }
     }
-    
+
     public void OnW(InputAction.CallbackContext value)
     {
-        if (value.started && _expeditionManaging.CoolTimes[playerNum, 1] <= 0 && _playerDataContainer.skillNums.Count >= 2)
+        if (value.started && _expeditionManaging.CoolTimes[playerNum, 1] <= 0 &&
+            _playerDataContainer.skillNums.Count >= 2)
         {
             _playerSkill.Skill(_playerDataContainer.skillNums[1]);
             _expeditionManaging.CoolTimes[playerNum, 1] = _playerDataContainer.finalCoolTimes[1]; // 쿨타임 적용
         }
     }
-    
+
     public void OnE(InputAction.CallbackContext value)
     {
-        if (value.started && _expeditionManaging.CoolTimes[playerNum, 2] <= 0 && _playerDataContainer.skillNums.Count >= 3)
+        if (value.started && _expeditionManaging.CoolTimes[playerNum, 2] <= 0 &&
+            _playerDataContainer.skillNums.Count >= 3)
         {
             _playerSkill.Skill(_playerDataContainer.skillNums[2]);
             _expeditionManaging.CoolTimes[playerNum, 2] = _playerDataContainer.finalCoolTimes[2]; // 쿨타임 적용
+        }
+    }
+
+    public void OnAttack(float attackTime)
+    {
+        _attackTime = attackTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("Grounded!");
+            _animator.SetBool("isJump", false);
+        }
+    }
+    
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("Grounded!");
+            _animator.SetBool("isJump", false);
         }
     }
 }
--- Assets/Scripts/Player/PlayerSkill.cs
+++ Assets/Scripts/Player/PlayerSkill.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 
 public class PlayerSkill : MonoBehaviour
 {
+    private Animator _animator;
+    private PlayerBehavior _playerBehavior;
+
+    public GameObject effect0;
+    public GameObject effect1;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _playerBehavior = GetComponent<PlayerBehavior>();
+    }
+
     public void Skill(int n)
     {
         switch (n)
@@ -17,6 +29,15 @@ public class PlayerSkill : MonoBehaviour
                 break;
             case 2:
                 Skill2();
+                break;
+            case 3:
+                StartCoroutine(Attack0());
+                break;
+            case 4:
+                StartCoroutine(Attack1());
+                break;
+            case 5:
+                StartCoroutine(Attack2());
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
@@ -36,5 +57,40 @@ public class PlayerSkill : MonoBehaviour
     public void Skill2()
     {
         Debug.Log("SKILL2 called!");
+    }
+
+    public IEnumerator Attack0()
+    {
+        _playerBehavior.OnAttack(28 / 60f);
+        _animator.SetTrigger("attack");
+        yield break;
+    }
+    
+    public IEnumerator Attack1()
+    {
+        _playerBehavior.OnAttack(28 / 60f);
+        _animator.SetTrigger("attack");
+
+        Vector3 pos = transform.position;
+        float dir = transform.localScale.x / Mathf.Abs(transform.localScale.x);
+        yield return new WaitForSeconds(0.3f);
+        for (int i = 0; i < 4; i++)
+        {
+            var effect = GameObject.Instantiate(effect0, 
+                pos + new Vector3((i+1) * 5, dir * 1, 0) * dir,
+                Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    
+    public IEnumerator Attack2()
+    {
+        _playerBehavior.OnAttack(28 / 60f);
+        _animator.SetTrigger("attack");
+        
+        yield return new WaitForSeconds(0.3f);
+        var effect = GameObject.Instantiate(effect1, 
+                transform.position + new Vector3(0, 2, 0),
+                Quaternion.identity);
     }
 }
